d119fab5556194fa737433e0268df86dff93f39e
PointVirgule07 <virgile.thabouret@robolyon.com>
2024-05-16 09:28:14 +0200

    .

--- bataille navale.py
+++ bataille navale.py
@@ -4,8 +4,8 @@ G2 = [["." for _ in range(10)] for _ in range(10)]
 
 """dictionnaire pour convertir les abréviations en nombre de cases occupées par le bateau et le nombre de bateaux de chaque types.
    (nombre de bateaux, nombre de cases occupées par le bateau)"""
-nombre_bateaux_J1 = {"PC": (1,5), "C": (1,4), "CT": (2,3), "T": (1,2)}
-nombre_bateaux_J2 = {"PC": (1,5), "C": (1,4), "CT": (2,3), "T": (1,2)}
+nombre_bateaux_J1 = {"pc": (1,5), "c": (1,4), "ct": (2,3), "t": (1,2)}
+nombre_bateaux_J2 = {"pc": (1,5), "c": (1,4), "ct": (2,3), "t": (1,2)}
 
 """transforme le nom des cases en leurs coordonées (x,y) (A0 -> (0,0), A1 -> (0,1), ..., J9 -> (9,9)"""
 dic = {chr(65+i)+str(j): (i,j) for i in range(10) for j in range(10)}
@@ -13,7 +13,7 @@ dic = {chr(65+i)+str(j): (i,j) for i in range(10) for j in range(10)}
 def affiche(G):
     """affiche la grille de jeu de la bataille navale
     G: list(list(str)) -> grille de jeu"""
-    print("  A B C D E F G H I J")
+    print("  A B c D E F G H I J")
     for i in range(10):
         print(i, end=' ')
         for j in range(10):
@@ -66,20 +66,20 @@ def touche_ou_plouf(G, emplacement, dic):
         G[x][y] = "O"
         return False
 
-while nombre_bateaux_J1["PC"][0] != 0 or nombre_bateaux_J1["C"][0] != 0 or nombre_bateaux_J1["CT"][0] != 0 or nombre_bateaux_J1["T"][0] != 0:
+while nombre_bateaux_J1["pc"][0] != 0 or nombre_bateaux_J1["c"][0] != 0 or nombre_bateaux_J1["ct"][0] != 0 or nombre_bateaux_J1["t"][0] != 0:
     print("\033[0;32m\nJoueur 1\033[0;0m\n")
     affiche(G1)
     emplacement = input("Entrez l'emplacement du bateau: ")
-    print(f"Les bateaux disponibles sont: PC {nombre_bateaux_J1['PC'][1]}, C {nombre_bateaux_J1['C'][1]}, CT {nombre_bateaux_J1['CT'][1]}, T {nombre_bateaux_J1['T'][1]}")
-    bateau = input("Entrez le type de bateau: ")
+    print(f"Les bateaux disponibles sont: PC {nombre_bateaux_J1['pc'][1]}, C {nombre_bateaux_J1['c'][1]}, CT {nombre_bateaux_J1['ct'][1]},  {nombre_bateaux_J1['t'][1]}")
+    bateau = input("Entrez le type de bateau: ").lower()
     direction = input("Entrez la direction du bateau (H pour horizontal, V pour vertical): ")
     placeBateau(G1, emplacement, bateau, direction, nombre_bateaux_J1)
 
-while nombre_bateaux_J2["PC"][0] != 0 or nombre_bateaux_J2["C"][0] != 0 or nombre_bateaux_J2["CT"][0] != 0 or nombre_bateaux_J2["T"][0] != 0:
+while nombre_bateaux_J2["pc"][0] != 0 or nombre_bateaux_J2["c"][0] != 0 or nombre_bateaux_J2["ct"][0] != 0 or nombre_bateaux_J2["t"][0] != 0:
     print("\033[0;32m\nJoueur 2\033[0;0m\n")
     affiche(G2)
     emplacement = input("Entrez l'emplacement du bateau: ")
-    print(f"Les bateaux disponibles sont: PC {nombre_bateaux_J2['PC'][1]}, C {nombre_bateaux_J2['C'][1]}, CT {nombre_bateaux_J2['CT'][1]}, T {nombre_bateaux_J2['T'][1]}")
+    print(f"Les bateaux disponibles sont: pc {nombre_bateaux_J2['pc'][1]}, c {nombre_bateaux_J2['c'][1]}, ct {nombre_bateaux_J2['ct'][1]}, t {nombre_bateaux_J2['t'][1]}")
     bateau = input("Entrez le type de bateau: ")
     direction = input("Entrez la direction du bateau (H pour horizontal, V pour vertical): ")
     placeBateau(G2, emplacement, bateau, direction, nombre_bateaux_J2)
