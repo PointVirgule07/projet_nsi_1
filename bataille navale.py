cbcf77851f4aa8b68b847cf4a6136a3a623125fd
PointVirgule07 <virgile.thabouret@robolyon.com>
2024-05-16 09:24:06 +0200

    placement dans la grille marche

--- bataille navale.py
+++ bataille navale.py
@@ -9,7 +9,6 @@ nombre_bateaux_J2 = {"PC": (1,5), "C": (1,4), "CT": (2,3), "T": (1,2)}
 
 """transforme le nom des cases en leurs coordonées (x,y) (A0 -> (0,0), A1 -> (0,1), ..., J9 -> (9,9)"""
 dic = {chr(65+i)+str(j): (i,j) for i in range(10) for j in range(10)}
-print(dic)
 
 def affiche(G):
     """affiche la grille de jeu de la bataille navale
@@ -29,39 +28,25 @@ def placeBateau(G, emplacement, bateau, direction, nombre_bateaux):
     direction: str -> direction du bateau
     nombre_bateaux: dict -> dictionnaire des bateaux
     return: bool -> True si le bateau a été placé, False sinon"""
+    taille = nombre_bateaux.get(bateau, (0, 0))[1]
+    x, y = dic.get(emplacement, (0, 0))
 
-    taille = nombre_bateaux_J1[bateau][1]
-    x,y = dic[emplacement][0],dic[emplacement][1]
-    if bateau not in nombre_bateaux_J1 :
-        print("bateau non valide")
+    if bateau not in nombre_bateaux or G[y][x] != "." or not estDansGrille(x, y) or direction not in {"H", "V"} or nombre_bateaux[bateau][0] == 0 or (direction == "V" and y + taille > 10) or (direction == "H" and x + taille > 10):
+        print("\033[1;31mPlacement invalide\033[1;37m")
         return False
-    elif G[x][y] != "." :
-        print("case déjà occupée")
-        return False
-    elif not estDansGrille(x,y) :
-        print("case hors grille")
-        return False
-    elif (direction != "H" and direction != "V"):
-        print("direction non valide")
-        return False
-    elif nombre_bateaux_J1[bateau][0] == 0:
-        print("plus de bateaux de ce type")
-        return False
-    else:       
-        if direction == "H":
-            if y+taille > 10 :
-                print("bateau hors grille")
-                return False
-            else :
-                for i in range(taille):
-                    G[x][y+i] = "B"
-        elif direction == "V":
-            if x+taille > 10 :
-                print("bateau hors grille")
-                return False
-            else :
-                for i in range(taille):
-                    G[x+i][y] = "B"
+
+    for i in range(taille):
+        if (direction == "V" and G[y + i][x] != ".") or (direction == "H" and G[y][x + i] != "."):
+            print("\033[1;31mCase déjà occupée\033[1;37m")
+            return False
+
+    for i in range(taille):
+        if direction == "V":
+            G[y + i][x] = "B"
+        else:
+            G[y][x + i] = "B"
+    nombre_bateaux[bateau] = (nombre_bateaux[bateau][0] - 1, nombre_bateaux[bateau][1])
+    return True
 
 def estDansGrille(x, y):
     """vérifie si les coordonnées sont dans la grille"""
@@ -81,11 +66,22 @@ def touche_ou_plouf(G, emplacement, dic):
         G[x][y] = "O"
         return False
 
-#while True:
+while nombre_bateaux_J1["PC"][0] != 0 or nombre_bateaux_J1["C"][0] != 0 or nombre_bateaux_J1["CT"][0] != 0 or nombre_bateaux_J1["T"][0] != 0:
+    print("\033[0;32m\nJoueur 1\033[0;0m\n")
     affiche(G1)
     emplacement = input("Entrez l'emplacement du bateau: ")
     print(f"Les bateaux disponibles sont: PC {nombre_bateaux_J1['PC'][1]}, C {nombre_bateaux_J1['C'][1]}, CT {nombre_bateaux_J1['CT'][1]}, T {nombre_bateaux_J1['T'][1]}")
     bateau = input("Entrez le type de bateau: ")
     direction = input("Entrez la direction du bateau (H pour horizontal, V pour vertical): ")
     placeBateau(G1, emplacement, bateau, direction, nombre_bateaux_J1)
+
+while nombre_bateaux_J2["PC"][0] != 0 or nombre_bateaux_J2["C"][0] != 0 or nombre_bateaux_J2["CT"][0] != 0 or nombre_bateaux_J2["T"][0] != 0:
+    print("\033[0;32m\nJoueur 2\033[0;0m\n")
+    affiche(G2)
+    emplacement = input("Entrez l'emplacement du bateau: ")
+    print(f"Les bateaux disponibles sont: PC {nombre_bateaux_J2['PC'][1]}, C {nombre_bateaux_J2['C'][1]}, CT {nombre_bateaux_J2['CT'][1]}, T {nombre_bateaux_J2['T'][1]}")
+    bateau = input("Entrez le type de bateau: ")
+    direction = input("Entrez la direction du bateau (H pour horizontal, V pour vertical): ")
+    placeBateau(G2, emplacement, bateau, direction, nombre_bateaux_J2)
+
 
